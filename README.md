dffb894ecfba31e413951f3bb5e79e53b739a329
Jayce Azua <jayce.azua@gmail.com>
2018-10-11 02:11:24 -0700

    finishing...

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Browser Web API Project
+
+
+### Features
+`Geolocation.getCurrentPosition()`
+- Determines the device's current location and gives back a Position object with the data.
+
+`Geolocation.watchPosition()`
+- Returns a long value representing the newly established callback function to be invoked whenever the device location changes.
+`Geolocation.clearWatch()`
+- Removes the particular handler previously installed using `watchPosition()`.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
-let sampleAPI = 'https://itunes.apple.com/us/rss/topmovies/limit=28/json';
-const title = document.getElementById('title');
-fetch(sampleAPI)
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.log(myJson)
-    title.innerHTML = myJson.feed.title.label
-  });
+const findLocation = document.getElementById('findLocation')
+const appID = 'iZ9pE45kdJ2H3zEt4b1Q'
+const appCode = `XlKLjYFIoG_rHJMygBYPMA`
+
+findLocation.onclick = (e) => {
+    geoFindMe()
+}
+function geoFindMe() {
+ const output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+      let latitude  = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      output.innerHTML =  `<p>Latitude is ${latitude} <br> Longitude is ${longitude}</p>`;
+  }
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating . . . </p>";
+  navigator.geolocation.getCurrentPosition(success, error);
+  /*
+  .getCurrentPosition() method is used to get the current position of the device.
+  */
+}
