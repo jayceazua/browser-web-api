cd58ee70b537e504a3939887fa8e11d583820c17
Jayce Azua <jayce.azua@gmail.com>
2018-10-10 21:52:08 -0700

    getting things done...

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 let sampleAPI = 'https://itunes.apple.com/us/rss/topmovies/limit=28/json';
-
+const title = document.getElementById('title');
 fetch(sampleAPI)
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    document.getElementById('demo').innerHTML = JSON.stringify(myJson);
+    console.log(myJson)
+    title.innerHTML = myJson.feed.title.label
   });
